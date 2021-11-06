02b1d0848c49eea4aab8eb9beef956ca9b785961
ArsenijeD <aco.degenek@gmail.com>
2021-11-06 22:49:41 +0100

    Add ngx-gridster

--- vehicle-manager-client/src/app/app.component.ts
+++ vehicle-manager-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CompactType, GridsterConfig, GridsterItem, GridType } from 'ngx-gridster';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'vehicle-manager-client';
+  options: GridsterConfig
+  dashboard: Array<GridsterItem>
+
+  ngOnInit() {
+    this.options = {
+      gridType: GridType.Fit,
+      displayGrid: CompactType.None,
+      draggable: {
+        enabled: true,
+        delayStart: 70 //for better user experience on touch screen
+      },
+      swap: true
+    };
+
+    this.dashboard = [
+      {cols: 1, rows: 1, y: 0, x: 0},
+      {cols: 1, rows: 1, y: 1, x: 0},
+      {cols: 1, rows: 1, y: 0, x: 1},
+      {cols: 1, rows: 1, y: 1, x: 1}
+    ];
+  }
 }
